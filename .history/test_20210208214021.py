3f72d2ad19798738369f9cec3ab37f6f9cca5448
heartwall-0 <1813763848@qq.com>
2021-02-08 21:49:55 +0800

    pygame

---
+++ .history/test_20210208214021.py
@@ -0,0 +1,8 @@
+'''
+Author: your name
+Date: 2021-02-08 21:40:20
+LastEditTime: 2021-02-08 21:40:20
+LastEditors: your name
+Description: In User Settings Edit
+FilePath: \pygame\test.py
+'''
---
+++ .history/test_20210208214023.py
@@ -0,0 +1,8 @@
+'''
+Author: your name
+Date: 2021-02-08 21:40:20
+LastEditTime: 2021-02-08 21:40:23
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \pygame\test.py
+'''
